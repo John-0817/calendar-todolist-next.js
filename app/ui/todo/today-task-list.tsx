317220fbe33cacbe05fc1823015bb69aed691297
John-0817 <96937877+John-0817@users.noreply.github.com>
2024-04-24 21:32:31 +0800

    2

--- app/ui/todo/today-task-list.tsx
+++ app/ui/todo/today-task-list.tsx
@@ -10,7 +10,7 @@ import Link from 'next/link';
 
 export default async function TaskListToday() {
   const taskForToday = await fetchTaskToday();
-  
+  console.log(taskForToday);
   return(
     <>
       {taskForToday.map((task) => {
