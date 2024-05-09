80b21af1b2097f5113338538c342b7ae9ff0b9aa
John-0817 <96937877+John-0817@users.noreply.github.com>
2024-05-09 14:32:24 +0800

    data fetching

--- app/lib/data.ts
+++ app/lib/data.ts
@@ -142,15 +142,15 @@ export async function fetchUpcomingTaskTomorrow() {
 export async function fetchUpcomingTaskThisWeek() {
   noStore();
 
-  const today = new Date();
-  const weekStart = today.getDate() - today.getDay();
-  const weekEnd = weekStart + 6;
-  const lastDay = new Date(today.setDate(weekEnd)).toLocaleDateString();
-  const firstDay = new Date(today.setDate(weekStart)).toLocaleDateString();
-  const firstDayArray = firstDay.split('/');
-  const lastDayArray = lastDay.split('/');
-  const firstDate = `${firstDayArray[2]}-${firstDayArray[1]}-${firstDayArray[0]}`;
-  const lastDate = `${lastDayArray[2]}-${lastDayArray[1]}-${lastDayArray[0]}`;
+  const date = new Date();
+  const month = date.getMonth();
+  const year = date.getFullYear();
+  const weekStart = date.getDate() - date.getDay() < 0 ? 1 : date.getDate() - date.getDay();
+  const weekEnd = date.getDate() + 6 - date.getDay() > new Date(year, month + 1, 0).getDate() 
+  ? new Date(year, month + 1, 0).getDate() : date.getDate() + 6 - date.getDay();
+
+  const firstDate = new Date(year, month, weekStart).toISOString();
+  const lastDate = new Date(year, month, weekEnd).toISOString();
 
   try {
     const data = await sql<Task>`
